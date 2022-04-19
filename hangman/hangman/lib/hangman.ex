6db526c77c021efdf954bd385001864fc36c2333
rafaelbiten <rafaelbiten@icloud.com>
2022-04-19 08:44:39 -0400

    [FEAT] Expose get_tally fn

--- hangman/hangman/lib/hangman.ex
+++ hangman/hangman/lib/hangman.ex
@@ -12,11 +12,16 @@ defmodule Hangman do
   def new_game, do: Agent.start_link(fn -> Game.new_game() end)
 
   @spec make_move(pid(), String.t()) :: tally
-  def make_move(agent, guess) do
-    Agent.update(agent, fn game -> Game.make_move(game, guess) end)
-    Agent.get(agent, fn game -> Game.to_public_tally(game) end)
+  def make_move(pid, guess) do
+    Agent.update(pid, fn game -> Game.make_move(game, guess) end)
+    get_tally(pid)
+  end
+
+  @spec get_tally(pid()) :: tally
+  def get_tally(pid) do
+    Agent.get(pid, fn game -> Game.to_public_tally(game) end)
   end
 
   @spec end_game(pid()) :: :ok
-  def end_game(agent), do: Agent.stop(agent)
+  def end_game(pid), do: Agent.stop(pid)
 end
