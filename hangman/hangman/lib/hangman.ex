c1605f119e64ede080ff6bb8b4039868a3979064
rafaelbiten <rafaelbiten@icloud.com>
2022-04-18 21:59:16 -0400

    [FEAT] Using an Agent to track the game state

--- hangman/hangman/lib/hangman.ex
+++ hangman/hangman/lib/hangman.ex
@@ -3,11 +3,20 @@ defmodule Hangman do
   The public API of the Hangman game
   """
 
+  use Agent
   alias Hangman.Impl.Game
 
-  @spec new_game :: Game.t()
-  defdelegate new_game, to: Game
+  @type tally :: Game.public_tally()
 
-  @spec make_move(Game.t(), String.t()) :: Game.t()
-  defdelegate make_move(game, guess), to: Game
+  @spec new_game() :: {:error, any} | {:ok, pid()}
+  def new_game, do: Agent.start_link(fn -> Game.new_game() end)
+
+  @spec make_move(pid(), String.t()) :: tally
+  def make_move(agent, guess) do
+    Agent.update(agent, fn game -> Game.make_move(game, guess) end)
+    Agent.get(agent, fn game -> Game.to_public_tally(game) end)
+  end
+
+  @spec end_game(pid()) :: :ok
+  def end_game(agent), do: Agent.stop(agent)
 end
