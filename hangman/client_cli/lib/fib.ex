bbf1ed5f41e2fdef78629a7088041b5843dfc2c4
rafaelbiten <rafaelbiten@icloud.com>
2022-04-20 22:59:55 -0400

    [FEAT] Challenge: Fibonacci of 100 (Cached fib)

---
+++ hangman/client_cli/lib/fib.ex
@@ -0,0 +1,73 @@
+defmodule Cache do
+  use Agent
+
+  @type key :: number
+  @type value :: number
+
+  @spec init :: {:error, any} | {:ok, pid}
+  def init, do: Agent.start_link(fn -> %{0 => 0, 1 => 1} end, name: __MODULE__)
+
+  @spec get(key) :: value | nil
+  def get(key), do: Agent.get(__MODULE__, fn cache -> Map.get(cache, key) end)
+
+  @spec put(key, value) :: :ok
+  def put(key, value), do: Agent.update(__MODULE__, fn cache -> Map.put(cache, key, value) end)
+end
+
+defmodule Fib do
+  @moduledoc """
+
+  Cached Fib to calculate large Fibonacci sequences without crashing the machine.
+
+  Challenge: Try calculating fib(100).
+  If it comes back without crashing your machine, and before the sun has expanded to consume the Earth, then your cache worked...
+
+  Usage:
+    > Fib.init
+    > Fib.of 500
+
+    cached 2: 1
+    cached 3: 2
+    ...
+    cached 499: 86168291600238450732788312165664788095941068326060883324529903470149056115823592713458328176574447204501
+    cached 500: 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125
+  """
+
+  @spec init :: {:error, any} | {:ok, pid}
+  def init, do: Cache.init()
+
+  @spec of(number) :: number
+  def of(0), do: 0
+  def of(1), do: 1
+  def of(n), do: cached(n)
+
+  defp cached(n) do
+    case Cache.get(n) do
+      nil ->
+        result = cache_or_fib(n - 1) + cache_or_fib(n - 2)
+        Cache.put(n, result)
+        IO.puts("cached #{n}: #{result}")
+        result
+
+      value ->
+        value
+    end
+  end
+
+  defp cache_or_fib(n) do
+    case Cache.get(n) do
+      nil ->
+        result = fib(n)
+        Cache.put(n, result)
+        IO.puts("cached #{n}: #{result}")
+        result
+
+      value ->
+        value
+    end
+  end
+
+  defp fib(0), do: 0
+  defp fib(1), do: 1
+  defp fib(n), do: cache_or_fib(n - 1) + fib(n - 2)
+end
