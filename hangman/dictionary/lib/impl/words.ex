b69b4e48808203f1e691ede3e7cd4edb22df0967
rafaelbiten <rafaelbiten@icloud.com>
2022-04-21 23:09:30 -0400

    [FIX] Resolve path to assets/words at compile time

--- hangman/dictionary/lib/impl/words.ex
+++ hangman/dictionary/lib/impl/words.ex
@@ -1,13 +1,15 @@
 defmodule Dictionary.Impl.Words do
-  @type t :: list(binary)
+  @cwd File.cwd!()
+
+  @type t :: list(String.t())
 
   @spec get_words :: t
   def get_words do
-    "assets/words.txt"
+    Path.join(@cwd, "/assets/words.txt")
     |> File.read!()
     |> String.split("\n", trim: true)
   end
 
-  @spec random_word(t) :: binary
+  @spec random_word(t) :: String.t()
   def random_word(words), do: Enum.random(words)
 end
