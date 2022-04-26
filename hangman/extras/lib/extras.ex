76bfb2f58512a3a77d2d2d24e90d77ea2af3a8e9
rafaelbiten <rafaelbiten@icloud.com>
2022-04-25 20:00:16 -0400

    [REFACT] Move fib challenge

---
+++ hangman/extras/lib/extras.ex
@@ -0,0 +1,18 @@
+defmodule Extras do
+  @moduledoc """
+  Documentation for `Extras`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Extras.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
