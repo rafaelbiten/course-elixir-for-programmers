794dc41b0c5dd5f17080b0d16a0608d8234ebeb0
rafaelbiten <rafaelbiten@icloud.com>
2022-05-23 11:27:02 -0400

    [REFACT] hangman_figures as a map

--- hangman/client_html/lib/impl/views/hangman_view.ex
+++ hangman/client_html/lib/impl/views/hangman_view.ex
@@ -1,74 +1,74 @@
 defmodule ClientHtml.Impl.HangmanView do
   use ClientHtml.Impl, :view
 
-  @hangman_figures [
-    ~S(
+  @hangman_figures_by_turns_left %{
+    7 => ~S(
+    +---+
+        |
+        |
+        |
+        |
+        |
+  =========),
+    6 => ~S(
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+  =========),
+    5 => ~S(
     +---+
     |   |
     O   |
-   /|\  |
-   / \  |
         |
-  ========= 7),
-    ~S(
+        |
+        |
+  =========),
+    4 => ~S(
     +---+
     |   |
     O   |
-   /|\  |
-   /    |
-        |
-  ========= 6),
-    ~S(
-    +---+
     |   |
-    O   |
-   /|\  |
         |
         |
-  ========= 5),
-    ~S(
+  =========),
+    3 => ~S(
     +---+
     |   |
     O   |
    /|   |
         |
         |
-  ========= 4),
-    ~S(
+  =========),
+    2 => ~S(
     +---+
     |   |
     O   |
-    |   |
+   /|\  |
         |
         |
-  ========= 3),
-    ~S(
+  =========),
+    1 => ~S(
     +---+
     |   |
     O   |
+   /|\  |
+   /    |
         |
-        |
-        |
-  ========= 2),
-    ~S(
+  =========),
+    0 => ~S(
     +---+
     |   |
+    O   |
+   /|\  |
+   / \  |
         |
-        |
-        |
-        |
-  ========= 1),
-    ~S(
-    +---+
-        |
-        |
-        |
-        |
-        |
-  ========= 0)
-  ]
+  =========)
+  }
 
   def draw_hangman(turns_left) when turns_left >= 0 and turns_left <= 7 do
-    Enum.at(@hangman_figures, turns_left)
+    @hangman_figures_by_turns_left[turns_left]
   end
 end
