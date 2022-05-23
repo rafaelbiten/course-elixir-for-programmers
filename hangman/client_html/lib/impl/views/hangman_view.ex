e28de831b39eaa07843d21fd6ba1eff98024090b
rafaelbiten <rafaelbiten@icloud.com>
2022-05-23 12:12:46 -0400

    [REFACT] Move hangman rendering into a helper mod

--- hangman/client_html/lib/impl/views/hangman_view.ex
+++ hangman/client_html/lib/impl/views/hangman_view.ex
@@ -1,76 +1,7 @@
 defmodule ClientHtml.Impl.HangmanView do
   use ClientHtml.Impl, :view
 
-  @hangman_figures_by_turns_left %{
-    7 => ~S(
-    +---+
-        |
-        |
-        |
-        |
-        |
-  =========),
-    6 => ~S(
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-  =========),
-    5 => ~S(
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-  =========),
-    4 => ~S(
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-  =========),
-    3 => ~S(
-    +---+
-    |   |
-    O   |
-   /|   |
-        |
-        |
-  =========),
-    2 => ~S(
-    +---+
-    |   |
-    O   |
-   /|\  |
-        |
-        |
-  =========),
-    1 => ~S(
-    +---+
-    |   |
-    O   |
-   /|\  |
-   /    |
-        |
-  =========),
-    0 => ~S(
-    +---+
-    |   |
-    O   |
-   /|\  |
-   / \  |
-        |
-  =========)
-  }
-
-  def render_hangman(turns_left) when turns_left >= 0 and turns_left <= 7 do
-    @hangman_figures_by_turns_left[turns_left]
-  end
+  defdelegate render_hangman(turns_left), to: ClientHtml.Impl.HangmanHelpers
 
   def render_controls(conn, state) when state in [:won, :lost] do
     button("Play Again", to: Routes.hangman_path(conn, :new))
