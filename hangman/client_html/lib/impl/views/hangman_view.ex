89a931cacdf76c8dc1e407f6360d5e84bf7d9c66
rafaelbiten <rafaelbiten@icloud.com>
2022-05-23 11:42:34 -0400

    [FEAT] Rendering controls

--- hangman/client_html/lib/impl/views/hangman_view.ex
+++ hangman/client_html/lib/impl/views/hangman_view.ex
@@ -68,7 +68,32 @@ defmodule ClientHtml.Impl.HangmanView do
   =========)
   }
 
-  def draw_hangman(turns_left) when turns_left >= 0 and turns_left <= 7 do
+  def render_hangman(turns_left) when turns_left >= 0 and turns_left <= 7 do
     @hangman_figures_by_turns_left[turns_left]
+  end
+
+  def render_controls(conn, state) when state in [:won, :lost] do
+    button("Play Again", to: Routes.hangman_path(conn, :new))
+  end
+
+  def render_controls(conn, _state) do
+    form_for(
+      conn,
+      Routes.hangman_path(conn, :update),
+      [as: "make_move", method: :put],
+      fn form ->
+        [
+          text_input(form, :guess,
+            autofocus: true,
+            pattern: "^[a-zA-Z]{1}$",
+            placeholder: "enter a single letter",
+            oninvalid: "setCustomValidity('enter a single letter')",
+            oninput: "setCustomValidity('')",
+            class: "hangman_input_letter"
+          ),
+          submit("Submit guess")
+        ]
+      end
+    )
   end
 end
