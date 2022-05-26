0b6a38a5dbae9d78035a4f8cb0ea1b4126bd8035
rafaelbiten <rafaelbiten@icloud.com>
2022-05-25 21:54:24 -0400

    [FEAT] Copying over the hangman_helper.ex

---
+++ hangman/client_live_view/lib/impl/views/hangman_helpers.ex
@@ -0,0 +1,72 @@
+defmodule ClientLiveViewWeb.Impl.HangmanHelpers do
+  @hangman_figures_by_turns_left %{
+    7 => ~S(
+    +---+
+        |
+        |
+        |
+        |
+        |
+  =========),
+    6 => ~S(
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+  =========),
+    5 => ~S(
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+  =========),
+    4 => ~S(
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+  =========),
+    3 => ~S(
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+  =========),
+    2 => ~S(
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+  =========),
+    1 => ~S(
+    +---+
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |
+  =========),
+    0 => ~S(
+    +---+
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |
+  =========)
+  }
+
+  def render_hangman(turns_left) when turns_left >= 0 and turns_left <= 7 do
+    @hangman_figures_by_turns_left[turns_left]
+  end
+end
