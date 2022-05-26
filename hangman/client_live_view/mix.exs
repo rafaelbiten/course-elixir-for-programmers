434e00b7f610625ed4f149c24ed8006fb85978b6
rafaelbiten <rafaelbiten@icloud.com>
2022-05-25 21:53:02 -0400

    [FEAT] Adding project dependencies

--- hangman/client_live_view/mix.exs
+++ hangman/client_live_view/mix.exs
@@ -43,7 +43,12 @@ defmodule ClientLiveView.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+
+      # local dependencies
+      {:hangman, path: "../hangman"}
     ]
   end
 
